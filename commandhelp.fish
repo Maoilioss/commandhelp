d320d08baf16c5a7119894492e5db386bacf82d8
Maoilioss <156782221+Maoilioss@users.noreply.github.com>
2025-03-29 12:25:56 +0100

    Add files via upload

---
+++ commandhelp.fish
@@ -0,0 +1,15 @@
+function commandhelp
+	echo '/////////////////////////////////////////////////'
+	echo
+	echo "Cheatsheet för bra-att-kunna kommandon"
+	echo
+	echo 'pacman -Ss : Söker efter paket.'
+	echo
+	echo 'pacman -Rns $(pacman -Qdtq) : Tar bort oanvända paket.'
+	echo
+	echo 'df -h : Visar hur mycket av din disk som används.'
+	echo
+	echo 'nvidia-smi: Visar ditt grafikkorts drivrutiner.'
+	echo
+	echo '/////////////////////////////////////////////////'
+end
